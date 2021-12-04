7722cac27b53ffb98c7b414832d9ed654f0c2fc8
IJAZ MOHAMMED <talktoijaz@gmail.com>
2021-12-04 13:59:52 +0000

    docker.sh edited online with Bitbucket

--- docker.sh
+++ docker.sh
@@ -1,10 +1,4 @@
 #bin/bash
-sudo docker -v
-sudo docker images
-sudo docker ps -a
-set +e
-            
-            
 containers=$(docker ps -a -q --filter="name=book")
  if [ ! -z $containers ]; then
   docker stop $containers;
